0315bb0677e1c944e35c7a608957f58c70db8867
JoyChudasama <joychudasama11@gmail.com>
2025-02-23 16:19:38 -0400

    created calculate_based_on_income tool

--- tool_calling.py
+++ tool_calling.py
@@ -1,6 +1,7 @@
 from langchain_ollama.chat_models import ChatOllama
 from langchain_core.tools import tool
 from langchain_core.messages import HumanMessage, ToolMessage
+import json
 
 @tool
 def multiply(a: int, b: int) -> int:
@@ -12,6 +13,28 @@ def add(a: int, b: int) -> int:
     """Adds two numbers."""
     return str(a + b)
 
+@tool
+def calculate_based_on_income(incomes:list[dict[str, str | float]], amount:float) -> list:
+    """Splits the given amount across incomes based on each income's percentage of the total income.
+    Args:
+        incomes: list of dictionaries with 'name' and 'salary' keys
+        amount: float
+    """
+
+    total_salary = sum(income['salary'] for income in incomes)
+    
+    if total_salary == 0:
+        return [{'name': income['name'], 'amount': 0} for income in incomes]
+    
+    result = []
+    
+    for income in incomes:
+        allocated_amount = (income['salary'] / total_salary) * amount
+        result.append({'name': income['name'], 'amount': allocated_amount})
+    
+    return json.dumps(result)
+
+
 def get_model(model_name="mistral:latest") -> ChatOllama:
     return ChatOllama(
         model=model_name,
@@ -20,13 +43,13 @@ def get_model(model_name="mistral:latest") -> ChatOllama:
 
 def main(user_query: str) -> str:  
     msgs = [user_query]
-    ollama = get_model().bind_tools(tools=[multiply, add])
+    ollama = get_model().bind_tools(tools=[multiply, add, calculate_based_on_income])
     ai_msg = ollama.invoke(input=msgs)
     msgs.append(ai_msg)
     print(ai_msg.tool_calls)
 
     for tool_call in ai_msg.tool_calls:
-        selected_tool = {"add": add, "multiply": multiply}[tool_call["name"].lower()]
+        selected_tool = {"add": add, "multiply": multiply, "calculate_based_on_income":calculate_based_on_income}[tool_call["name"].lower()]
         tool_output = selected_tool.invoke(tool_call["args"])
         msgs.append(ToolMessage(tool_output, tool_call_id=tool_call["id"]))
 
@@ -36,5 +59,6 @@ def main(user_query: str) -> str:
 
 if __name__ == "__main__":
     user_query=input('Query: ')
+    
     res = main(HumanMessage(content=user_query.strip()))
     print(res)
