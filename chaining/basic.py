bcea09456b52838f2c91f485f244170c3b03175c
JoyChudasama <joychudasama11@gmail.com>
2025-03-07 07:57:54 -0400

    basic chaining with langchain expression language

---
+++ chaining/basic.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+from langchain_ollama.chat_models import ChatOllama
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
+
+load_dotenv()
+
+def get_model(model_name="mistral:latest") -> ChatOllama:
+    return ChatOllama(
+        model=model_name,
+        temperature=0.75,
+    )
+
+def get_prompt() -> ChatPromptTemplate:
+    messages = [
+        ("system", "You are a comedian who tells jokes about {topic}."),
+        ("human", "Tell me {joke_count} jokes."),
+    ]
+    
+    return ChatPromptTemplate.from_messages(messages)
+
+def main() -> str:
+    prompt = get_prompt()
+    model = get_model()
+    chain = prompt | model | StrOutputParser()
+
+    return chain.invoke({"topic": "ai", "joke_count": 3})
+
+if __name__ == "__main__":
+    res = main()
+    print(res, '\n')
