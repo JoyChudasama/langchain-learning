ebd343d284d3a978ba95d193a7bedd0801e712ad
JoyChudasama <joychudasama11@gmail.com>
2025-03-11 07:46:45 -0300

    learned about embedding, text splitter, and vector store db; adde new books; refactored rag/basic.py

--- rag/basic.py
+++ rag/basic.py
@@ -1,41 +1,58 @@
 import os
-
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma
 from langchain_ollama import OllamaEmbeddings
 
-current_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(current_dir, "books", "book.txt")
-persistent_directory = os.path.join(current_dir, "db", "chroma_db")
+def init_db(file_path:str, persistent_directory:str,)->Chroma:   
 
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
 
+    if os.path.exists(persistent_directory):
+        print("\n--- Loading existing ChromaDB ---")
+        db = Chroma(persist_directory=persistent_directory, embedding_function=embeddings)
+    else:
+        print("\n--- Processing and embedding documents ---")
+        loader = TextLoader(file_path, encoding="utf-8")
+        documents = loader.load()
 
-if not os.path.exists(file_path):
-    raise FileNotFoundError(
-        f"The file {file_path} does not exist. Please check the path."
+        text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        docs = text_splitter.split_documents(documents)
+
+        db = Chroma.from_documents(docs, embeddings, persist_directory=persistent_directory)
+        db.persist() 
+    
+    return db
+
+def releavent_docs(query:str, db:Chroma)->list:
+    retriever = db.as_retriever(
+        search_type="similarity_score_threshold",
+        search_kwargs={"k": 3, "score_threshold": 0.4},
     )
 
-loader = TextLoader(file_path, encoding="utf-8")
-documents = loader.load()
+    return retriever.invoke(query)
 
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-docs = text_splitter.split_documents(documents)
+def main(user_query:str, db:Chroma)->list:
+    docs = releavent_docs(user_query, db)
 
-embeddings = OllamaEmbeddings(model="gemma2:2b")  
+    print("\n--- Relevant Documents ---")
+    for i, doc in enumerate(docs, 1):
+        print(f"Document {i}:\n{doc.page_content}\n")
+        if doc.metadata:
+            print(f"Source: {doc.metadata.get('source', 'Unknown')}\n")
 
-db = Chroma.from_documents(docs, embeddings, persist_directory=persistent_directory)
 
-query = "alcoholic"
+if __name__ == "__main__":
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, "books", "book_of_baseball.txt")
+    persistent_directory = os.path.join(current_dir, "db", "chroma_db")
+    db = init_db(file_path, persistent_directory)
 
-retriever = db.as_retriever(
-    search_type="similarity_score_threshold",
-    search_kwargs={"k": 3, "score_threshold": 0.45},
-)
-relevant_docs = retriever.invoke(query)
-
-print("\n--- Relevant Documents ---")
-for i, doc in enumerate(relevant_docs, 1):
-    print(f"Document {i}:\n{doc.page_content}\n")
-    if doc.metadata:
-        print(f"Source: {doc.metadata.get('source', 'Unknown')}\n")
+    while True:
+        user_query = input('\nQuery: ').strip()
+        if not user_query or user_query.lower() == 'qq':
+            print("\n--- Exiting ---")
+            break
+        print("\n--- Thinking ---")
+        res = main(user_query, db)
+        print(res, '\n')
